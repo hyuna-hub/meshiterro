e1809defe2239e0c804f7aad7dc05d8cf7582b37
hyuna-hub <61468181+hyuna-hub@users.noreply.github.com>
2020-03-15 18:45:01 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '29a1837a06bc5846bebdf1e3006bb08766b587f594cabdd7b1e207efa416a7fd045cf671a3aadc5b669ba2f1a5ca5674ad9def708082c2da4b43d91b21ca281c'
